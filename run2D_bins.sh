584df223cf565e823077abf301fd52736ac1cc74
Malik <malik.hassanaly@gmail.com>
2022-12-13 16:19:18 -0700

    make sure bin method works in parallel

--- run2D_bins.sh
+++ run2D_bins.sh
@@ -1,8 +1,5 @@
 # Run the downsampling script
-mpiexec -np 1 python main_iterative.py input2D_bins
-
-# Plot the loss
-python plotLoss.py input2D_bins
+mpiexec -np 4 python main_iterative.py input2D_bins
 
 # Plot the sampling results
 python visualizeDownSampled_subplots.py input2D_bins
