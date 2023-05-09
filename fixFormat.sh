75dfcac25f9a514d63a76bc63dabfc0e5925cc79
Malik <malik.hassanaly@gmail.com>
2023-05-09 11:16:19 -0600

    adding linter

--- fixFormat.sh
+++ fixFormat.sh
@@ -1,7 +1,4 @@
-poetry update
-poetry run isort *py
-poetry run black *py
-poetry run isort utils
-poetry run black utils
-poetry run isort data-efficientML
-poetry run black data-efficientML
+source .github/linters/formatting.sh
+
+format *.py
+format utils
