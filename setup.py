fcf4b8d9d6d32c2c3d48093264257e823b1eaf5e
Malik <malik.hassanaly@gmail.com>
2023-11-30 09:31:49 -0700

    make sure parallel test runs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="uips",
-    version="0.0.5",
+    version="0.0.6",
     description="Reduce a large and high-dimensional dataset by downselecting data uniformly in phase space",
     url="https://github.com/NREL/Phase-space-sampling",
     author="Malik Hassanaly",
