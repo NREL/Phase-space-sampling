aa63fef5602e5a62ff313a41e04efdf92d3cd38b
Malik <malik.hassanaly@gmail.com>
2023-11-30 09:42:16 -0700

    update tutorials

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ from prettyPlot.plotting import plt, pretty_labels, pretty_legend
 
 from uips import UIPS_INPUT_DIR
 from uips.wrapper import downsample_dataset_from_input
+import uips.utils.parallel as par
 
 ndat = int(1e5)
 nSampl = 100
@@ -42,26 +43,26 @@ inpt["dataFile"] = "norm.npy"
 inpt["nWorkingData"] = f"{ndat} {ndat}"
 inpt["nEpochs"] = f"5 20"
 inpt["nSamples"] = f"{nSampl}"
+
 best_files = downsample_dataset_from_input(inpt)
 
 
-downsampled_ds = {}
-for nsamp in best_files:
-    downsampled_ds[nsamp] = np.load(best_files[nsamp])["data"]
-
-fig = plt.figure()
-plt.plot(ds[:, 0], ds[:, 1], "o", color="k", label="full DS")
-plt.plot(
-    downsampled_ds[nSampl][:, 0],
-    downsampled_ds[nSampl][:, 1],
-    "o",
-    color="r",
-    label="downsampled",
-)
-pretty_labels("", "", 14)
-pretty_legend()
-plt.savefig("normal_downsample.png")
-
+if par.irank == par.iroot:
+    downsampled_ds = {}
+    for nsamp in best_files:
+        downsampled_ds[nsamp] = np.load(best_files[nsamp])["data"]
+    fig = plt.figure()
+    plt.plot(ds[:, 0], ds[:, 1], "o", color="k", label="full DS")
+    plt.plot(
+        downsampled_ds[nSampl][:, 0],
+        downsampled_ds[nSampl][:, 1],
+        "o",
+        color="r",
+        label="downsampled",
+    )
+    pretty_labels("", "", 14)
+    pretty_legend()
+    plt.savefig("normal_downsample.png")
 ```
 
 ## Purpose
--- tutorials/uips_normal.py
+++ tutorials/uips_normal.py
@@ -4,6 +4,7 @@ import numpy as np
 from prettyPlot.parser import parse_input_file
 from prettyPlot.plotting import plt, pretty_labels, pretty_legend
 
+import uips.utils.parallel as par
 from uips import UIPS_INPUT_DIR
 from uips.wrapper import downsample_dataset_from_input
 
@@ -17,22 +18,23 @@ inpt["dataFile"] = "norm.npy"
 inpt["nWorkingData"] = f"{ndat} {ndat}"
 inpt["nEpochs"] = f"5 20"
 inpt["nSamples"] = f"{nSampl}"
+
 best_files = downsample_dataset_from_input(inpt)
 
 
-downsampled_ds = {}
-for nsamp in best_files:
-    downsampled_ds[nsamp] = np.load(best_files[nsamp])["data"]
-
-fig = plt.figure()
-plt.plot(ds[:, 0], ds[:, 1], "o", color="k", label="full DS")
-plt.plot(
-    downsampled_ds[nSampl][:, 0],
-    downsampled_ds[nSampl][:, 1],
-    "o",
-    color="r",
-    label="downsampled",
-)
-pretty_labels("", "", 14)
-pretty_legend()
-plt.savefig("normal_downsample.png")
+if par.irank == par.iroot:
+    downsampled_ds = {}
+    for nsamp in best_files:
+        downsampled_ds[nsamp] = np.load(best_files[nsamp])["data"]
+    fig = plt.figure()
+    plt.plot(ds[:, 0], ds[:, 1], "o", color="k", label="full DS")
+    plt.plot(
+        downsampled_ds[nSampl][:, 0],
+        downsampled_ds[nSampl][:, 1],
+        "o",
+        color="r",
+        label="downsampled",
+    )
+    pretty_labels("", "", 14)
+    pretty_legend()
+    plt.savefig("normal_downsample.png")
