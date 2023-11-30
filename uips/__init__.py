b0c26c82b6dd71af23c512eccea9ae780c85d6d8
Malik <malik.hassanaly@gmail.com>
2023-11-30 09:23:07 -0700

    move inputs to package

--- uips/__init__.py
+++ uips/__init__.py
@@ -2,7 +2,7 @@
 import os
 
 UIPS_DIR = os.path.dirname(os.path.realpath(__file__))
-UIPS_INPUT_DIR = os.path.join(UIPS_DIR, "../inputs")
+UIPS_INPUT_DIR = os.path.join(UIPS_DIR, "inputs")
 UIPS_DATA_DIR = os.path.join(UIPS_DIR, "../data")
 UIPS_DOC_DIR = os.path.join(UIPS_DIR, "../documentation")
 UIPS_ML_DIR = os.path.join(UIPS_DIR, "../data-efficientML")
