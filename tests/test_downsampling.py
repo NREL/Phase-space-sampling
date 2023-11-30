3671b92ed7938def361179bfcb07e72f6220b872
Malik <malik.hassanaly@gmail.com>
2023-11-30 09:13:03 -0700

    make sure test can run

--- tests/test_downsampling.py
+++ tests/test_downsampling.py
@@ -1,7 +1,10 @@
 import os
 
 from uips import UIPS_INPUT_DIR
-from uips.wrapper import downsample_dataset_from_input_file
+from uips.wrapper import (
+    downsample_dataset_from_input,
+    downsample_dataset_from_input_file,
+)
 
 
 def test_nf_input():
@@ -12,3 +15,8 @@ def test_nf_input():
 def test_bins_input():
     input_file = os.path.join(UIPS_INPUT_DIR, "input_test_bins")
     downsample_dataset_from_input_file(input_file)
+
+
+if __name__ == "__main__":
+    test_nf_input()
+    test_bins_input()
