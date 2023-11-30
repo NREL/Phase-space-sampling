217fc3f081225cac0eb3e31d9e9dd54baccee6be
Malik <malik.hassanaly@gmail.com>
2023-11-30 08:59:55 -0700

    fix tests

--- tests/test_downsampling.py
+++ tests/test_downsampling.py
@@ -1,14 +1,14 @@
 import os
 
 from uips import UIPS_INPUT_DIR
-from uips.wrapper import downsample_dataset_from_input
+from uips.wrapper import downsample_dataset_from_input_file
 
 
 def test_nf_input():
     input_file = os.path.join(UIPS_INPUT_DIR, "input_test")
-    downsample_dataset_from_input(input_file)
+    downsample_dataset_from_input_file(input_file)
 
 
 def test_bins_input():
     input_file = os.path.join(UIPS_INPUT_DIR, "input_test_bins")
-    downsample_dataset_from_input(input_file)
+    downsample_dataset_from_input_file(input_file)
