5a29b22ef42af64b27a195483c566ef05a8be656
Malik <malik.hassanaly@gmail.com>
2023-11-29 17:41:16 -0700

    rename parallel test

---
+++ tests/test_parallel.py
@@ -0,0 +1,17 @@
+import argparse
+
+from phaseSpaceSampling.wrapper import downsample_dataset_from_input
+
+parser = argparse.ArgumentParser(description="Downsampler")
+parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    metavar="",
+    required=False,
+    help="Input file",
+    default="input",
+)
+args, unknown = parser.parse_known_args()
+
+downsample_dataset_from_input(args.input)
