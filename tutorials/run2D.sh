d0005bca699f9b7fe7f6045a1cca2d6cd42b20b6
Malik <malik.hassanaly@gmail.com>
2023-11-29 17:43:58 -0700

    update tutorials

--- tutorials/run2D.sh
+++ tutorials/run2D.sh
@@ -1,5 +1,5 @@
 # Run the downsampling script
-mpiexec -np 4 python main.py -i input2D
+mpiexec -np 4 python tests/test_parallel.py -i input2D
 
 # Plot the loss
 python postProcess/plotLoss.py -i input2D
--- tutorials/run2D_bins.sh
+++ tutorials/run2D_bins.sh
@@ -1,5 +1,5 @@
 # Run the downsampling script
-mpiexec -np 4 python main.py -i input2D_bins
+mpiexec -np 4 python tests/test_parallel.py -i input2D_bins
 
 # Plot the sampling results
 python postProcess/visualizeDownSampled_subplots.py -i input2D_bins
