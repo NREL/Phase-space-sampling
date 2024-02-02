26b75d5ed5d19853fee940fbdbbfca5ad54bcb58
Malik <malik.hassanaly@gmail.com>
2024-02-02 10:08:54 -0700

    add version management

--- uips/__init__.py
+++ uips/__init__.py
@@ -2,6 +2,8 @@
 
 import os
 
+from uips.version import __version__
+
 UIPS_DIR = os.path.dirname(os.path.realpath(__file__))
 UIPS_INPUT_DIR = os.path.join(UIPS_DIR, "inputs")
 UIPS_DATA_DIR = os.path.join(UIPS_DIR, "../data")
