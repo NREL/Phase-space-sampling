c25b58424eca6eefaaee5abcf16d366d7de15ea2
Malik <malik.hassanaly@gmail.com>
2024-02-02 10:23:36 -0700

    explicit package in setup for pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ import os
 
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "requirements.txt")) as f:
-    install_requires = f.readlines()
-
 setup(
     name="uips",
     version="0.1.0",
@@ -21,8 +16,15 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.10",
     ],
-    package_data={"": ["input2D", "input2D_bins"]},
+    package_data={"": ["input2D", "input2D_bins", "requirements.txt"]},
     include_package_data=True,
     python_requires=">=3.10",
-    install_requires=install_requires,
+    install_requires=[
+        "openmpi",
+        "torch",
+        "numpy",
+        "mpi4py",
+        "scikit-learn",
+        "prettyPlot>=0.0.25",
+    ],
 )
